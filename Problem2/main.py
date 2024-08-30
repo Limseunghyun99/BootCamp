e4a69d5d09aff5ce034c29fe387a19d9082d1275
sinam7 <wjdtjddjs7@gmail.com>
2024-08-30 15:30:24 +0900

    test: 테스트 코드 추가
        # Case 1: 숫자 리스트가 아닌 경우
        # Case 2: 리스트가 아닌 경우
        # Case 3: 중복된 값만 존재하는 리스트인 경우
        # Case 4: 교집합이 없는 경우
        # Case 5: 교집합이 전체인 경우
        # Case 6: 중복 제거에 실패하는 경우 (1)
        # Case 7: 중복 제거에 실패하는 경우 (2)

--- Problem2/main.py
+++ Problem2/main.py
@@ -25,3 +25,43 @@ if __name__ == '__main__':
     emptyArr.set([1, 2, 3, 3, 4], [2, 3, 4])
 
     print(arr.uniquify([1, 2, 3, 3, 4]))
+
+    # Case 1: 숫자 리스트가 아닌 경우
+    try:
+        arr = ArrayUtil(['1', '2', '3', '3', '4'], ['2', '3', '4'])
+        exit(1)
+    except ValueError:
+        pass
+
+    # Case 2: 리스트가 아닌 경우
+    try:
+        arr = ArrayUtil(1, 2)
+        exit(1)
+    except SyntaxError:
+        pass
+
+    # Case 3: 중복된 값만 존재하는 리스트인 경우
+    if arr.uniquify([1, 1, 1, 1, 1]) != [1]:
+        exit(1)
+
+    # Case 4: 교집합이 없는 경우
+    arr = ArrayUtil([1, 2, 3], [4, 5])
+    if arr.intersect() != []:
+        exit(1)
+
+    # Case 5: 교집합이 전체인 경우
+    arr = ArrayUtil([1, 2, 3], [1, 2, 3])
+    if arr.intersect() != [1, 2, 3]:
+        exit(1)
+
+    # Case 6: 중복 제거에 실패하는 경우 (1)
+    arr = ArrayUtil([1, 2, 3], [4, 5])
+    if arr.union() != [1, 2, 3, 4, 5]:
+        exit(1)
+
+    # Case 7: 중복 제거에 실패하는 경우 (2)
+    arr = ArrayUtil([1, 2, 3], [1, 2, 3])
+    if arr.union() != [1, 2, 3]:
+        exit(1)
+
+    print("Test OK")
