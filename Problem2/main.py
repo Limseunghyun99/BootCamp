eecc4541102436801d9db3bb653b3601f94317df
jsline59 <tnghks481@gmail.com>
2024-08-30 15:48:04 +0900

    합집합 교집합 추가 및 입력 검증 추가
    ArrayUtil 클래스에 intersect, union 메소드를 추가였습니다. 그리고 __checkIntArray 메소드로 입력 배열의 타입 검증을 추가하였습니다.

--- Problem2/main.py
+++ Problem2/main.py
@@ -2,22 +2,41 @@ class ArrayUtil:
     arr1 = []
     arr2 = []
 
+    def __checkIntArray(self, arr):
+        if type(arr) != list:
+            raise SyntaxError
+        for i in arr:
+            if type(i) != int:
+                raise ValueError
+
     def __init__(self, arr1=None, arr2=None):
-        if arr2 is None:
-            arr2 = []
         if arr1 is None:
             arr1 = []
-
-        self.arr1 = arr1
-        self.arr2 = arr2
+        if arr2 is None:
+            arr2 = []
+        self.set(arr1, arr2)
 
     def set(self, arr1, arr2):
+        self.__checkIntArray(arr1)
+        self.__checkIntArray(arr2)
         self.arr1 = arr1
         self.arr2 = arr2
 
     def uniquify(self, arr):
+        self.__checkIntArray(arr)
         return list(set(arr))
 
+    def intersect(self):
+        arr1_set = set(self.arr1)
+        arr2_set = set(self.arr2)
+
+        return list(arr1_set & arr2_set)
+
+    def union(self):
+        arr1_set = set(self.arr1)
+        arr2_set = set(self.arr2)
+
+        return list(arr1_set | arr2_set)
 
 if __name__ == '__main__':
     arr = ArrayUtil([1, 2, 3, 3, 4], [2, 3, 4])
@@ -36,32 +55,32 @@ if __name__ == '__main__':
     # Case 2: 리스트가 아닌 경우
     try:
         arr = ArrayUtil(1, 2)
-        exit(1)
+        exit(2)
     except SyntaxError:
         pass
 
     # Case 3: 중복된 값만 존재하는 리스트인 경우
     if arr.uniquify([1, 1, 1, 1, 1]) != [1]:
-        exit(1)
+        exit(3)
 
     # Case 4: 교집합이 없는 경우
     arr = ArrayUtil([1, 2, 3], [4, 5])
     if arr.intersect() != []:
-        exit(1)
+        exit(4)
 
     # Case 5: 교집합이 전체인 경우
     arr = ArrayUtil([1, 2, 3], [1, 2, 3])
     if arr.intersect() != [1, 2, 3]:
-        exit(1)
+        exit(5)
 
     # Case 6: 중복 제거에 실패하는 경우 (1)
     arr = ArrayUtil([1, 2, 3], [4, 5])
     if arr.union() != [1, 2, 3, 4, 5]:
-        exit(1)
+        exit(6)
 
     # Case 7: 중복 제거에 실패하는 경우 (2)
     arr = ArrayUtil([1, 2, 3], [1, 2, 3])
     if arr.union() != [1, 2, 3]:
-        exit(1)
+        exit(7)
 
     print("Test OK")
