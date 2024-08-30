4d521b7359adabfcabd691b1ad0a3ea6e9849b5c
jsline59 <tnghks481@gmail.com>
2024-08-30 15:03:57 +0900

    입력 조건 검증로직 추가
    입력 N이 정수인지, 20자리 이상이 아닌지를 검사하고, 아닐 경우 경고 메세지를 출력합니다. 만약 입력 오류가 3회 이상 발생한 경우 오류 메세지를 출력한 후 종료합니다. 음수인 경우에도 처리할 수 있도록 수정하였습니다.

--- Problem1/main.py
+++ Problem1/main.py
@@ -1,12 +1,53 @@
-def function():
+def input_func():
+    raw_input = input()
+    try:
+        raw_input = int(raw_input)
+    except ValueError:
+        print("입력은 정수여야 합니다.")
+        return None
+    
+    raw_input = str(raw_input)
+    
+    c = 0
+    if raw_input[0] == '-':
+        c = len(raw_input[1:])
+    else:
+        c = len(raw_input)
+    
+    if c > 20:
+        print("정수 N의 자릿수는 최대 20자리입니다.")
+        return None
+
+    return raw_input
+
+def function(raw_string):
     ret = ""
-    for i, e in enumerate((raw_string:=input())[::-1], start=1):
+    neg_flag = False
+    if raw_string[0] == '-':
+        neg_flag = True
+        raw_string = raw_string[1:]
+
+    for i, e in enumerate(raw_string[::-1], start=1):
         ret += e + ("," if i % 3 == 0 else "")
     ret = ret[::-1]
+
     if ret[0] == ',':
         ret = ret[1:]
+    
+    if neg_flag:
+        ret = '-' + ret
+
     print(ret)
 
 
 if __name__ == '__main__':
-    function()
+    c = 0
+    while c < 3:
+        a = input_func()
+        if a == None:
+            c += 1
+            continue
+        else:
+            function(a)
+            exit(0)
+    print("연속으로 3번 이상 잘못된 입력입니다.")
